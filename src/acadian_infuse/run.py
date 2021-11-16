28456fecd60ac600ac97f1ae7a4d1d4d2946bed7
Manyu Lakhotia <lakemonyew@gmail.com>
2021-11-15 19:13:11 -0600

    Adds script bin,csv writes,ploting,limiting data acadian_infuse

--- src/acadian_infuse/run.py
+++ src/acadian_infuse/run.py
@@ -4,7 +4,6 @@ import re
 import os
 import pandas as pd
 import numpy as np
-from py_linq import Enumerable
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -15,18 +14,20 @@ u.set_full_paths(config, this_dir)
 csv_loc = config['file_locations']['file_data']
 df: pd.DataFrame = pd.read_csv(csv_loc)  # type: ignore
 
-bins = np.linspace(0, 125, 10)
-print(bins)
+upper_line_count_limit = 125
+bins = np.linspace(0, upper_line_count_limit, 10)
 df['line_count_cat'] = pd.cut(df['line_count'], bins=bins)
 
 
 def file_type_binner(file_name):
-  return 'react' if re.search(
+  return 'config' if re.search(
+      '(\\.json$|ignore|config|docker|gradle|setup)', file_name)\
+      else 'react' if re.search(
       '\\.(tsx)$', file_name)\
-      else 'typescript' if re.search(
-      '\\.(ts)$', file_name)\
-      else 'config' if re.search(
-      '(\\.json$|ignore|docker)', file_name)\
+      else 'script' if re.search(
+      '\\.(bat|sh)$', file_name)\
+      else 'general_code' if re.search(
+      '\\.(ts|js|m)$', file_name)\
       else 'resource'
 
 
@@ -37,13 +38,25 @@ def file_ext_binner(file_name):
 df['file_type'] = df['file_name'].apply(file_type_binner)
 df['file_ext'] = df['file_name'].apply(file_ext_binner)
 
+df.to_csv(config['file_locations']['output_data'], index=False);
+
 pd.set_option('display.max_rows', df.shape[0] + 1)
 pd.set_option('display.max_columns', 10000)
 pd.set_option('display.max_colwidth', 200)
 
-print(df.sample(5))
+# print(df.sample(5))
 
-sns.set_theme()
+print(df['file_type'].value_counts())
+print(df.groupby('file_type').agg(['mean']))
+# print(df[df['file_type'] == 'config'])
 
-aplot = sns.countplot(y='line_count_cat', data=df, hue='file_type')
+# Limits data for charting purposes
+df = df[~(df['line_count'] > upper_line_count_limit)]
+print(df)
+# sns.set_theme()
+
+# aplot = sns.countplot(y='line_count_cat', data=df, hue='file_type')
+# plt.show()
+
+aplot = sns.boxplot(y='file_type', x='line_count', data=df)
 plt.show()
