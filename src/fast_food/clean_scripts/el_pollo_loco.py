10e4de7b5d6fefdf64cab4ceaebfa43d4e04cb24
Manyu Lakhotia <lakemonyew@gmail.com>
2022-04-27 08:31:13 -0500

    ~ to el_pollo clean script

--- src/fast_food/clean_scripts/el_pollo_loco.py
+++ src/fast_food/clean_scripts/el_pollo_loco.py
@@ -14,32 +14,30 @@ csv_loc = config['file_locations']['raw_el_pollo_loco']
 
 df: pd.DataFrame = pd.read_csv(csv_loc)  # type: ignore
 df.columns = df.columns.str.lower()
+pd.set_option('display.max_rows', df.shape[0] + 1)
+pd.set_option('display.max_columns', df.shape[1] + 1)
 
 for label in ['fat', 'protein', 'carbs']:
   df[label] = df[label].str.split('g', expand=True)[0]
+  df[f'{label}_units'] = 'g'
 
 df['calories'] = df.calories.astype('Int64')
 df['category'] = df.main_food_link
 df.drop(['web-scraper-order', 'web-scraper-start-url', 'main_food_link', 'main_food_link-href'], inplace=True, axis=1)
 df['company'] = 'El Pollo Loco'
-df.dropna(inplace=True, axis=0, subset=['calories'])
-df['serving_size_units'] = df.serving_size.str.split(' ', expand=True)[1]
-df['serving_size'] = df.serving_size.str.split(' ', expand=True)[0]
+df.dropna(subset=['calories'], axis=0, inplace=True)
+df[['serving_size','serving_size_units']] = df.serving_size.str.split(' ', expand=True)
 
-pd.set_option('display.max_rows', df.shape[0] + 1)
-pd.set_option('display.max_columns', df.shape[1] + 1)
-
-df['fat_units'] = 'g'
-df['protein_units'] = 'g'
-df['carb_units'] = 'g'
-
-ps = (lambda pdf: Enumerable([
-  lambda: pdf.columns,
-  lambda: pdf.serving_size_units.value_counts(dropna=False),
-  lambda: pdf.sample(10),
-  # lambda: pdf[['food', 'cal_per_gram', 'category']].groupby('category').food.agg(list),
-  # lambda: pdf.category.value_counts(),
-]))(df)
-u.foreach(lambda f: print(f()), ps)
+ps = [
+  # lambda: df.columns,
+  lambda: df.sample(5),
+  lambda: df.groupby(['category'])['calories'].mean().reset_index(),
+  # lambda: df.serving_size_units.value_counts(dropna=False),
+  # lambda: df.category.value_counts().nlargest(3).index,
+  # lambda: df.sample(5),
+  # lambda: df[['food', 'cal_per_gram', 'category']].groupby('category').food.agg(list),
+]
+for p in ps:
+  print(p())
 
 # df.to_csv(config['file_locations']['clean_el_pollo_loco'], index=False)
